20eeb0c46a051b45b3b385c96aa8881f758c4b84
kubalaj <joseph.kubala@colorado.edu>
2013-11-20 17:05:38 -0800

    init rails app

---
+++ kayakclubWP/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KayakclubWP::Application.config.secret_token = '7516fe64907a8bbffeb357bad55938c48fbe3780cb0a3c06e6a9d98cb8c301edd1bb7f1baf0390fdcfbdf2bc021a043f3b1d2ec62f0bfbaddb9ef6dfbc320883'
