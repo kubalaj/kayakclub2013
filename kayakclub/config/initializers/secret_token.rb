e0b7d53bf3dbe58bab14ed4d8b86745c7ca441d8
Jacob <jacob@wolniewicz.com>
2013-12-01 19:53:41 -0800

    removing directory

--- kayakclub/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Kayakclub::Application.config.secret_token = '2ee0c6ce97a8cc2cab31ad23bc9dfd5b99117f7fde9ca5e8c60ede1a5bc26e90c0aa1d3d0bff9d372699806f1b6c2f0cf1d518a38038e8edf16d97f9c1287d4e'
